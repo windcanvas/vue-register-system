263f4a8be317f97825a2ea5e1f6faa5ba47ff08a
李其龙 <liql@fun.tv>
2019-01-31 16:00:41 +0800

    项目完成

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<template>
+  <div id="app">
+    <!--左侧导航-->
+    <left-nav></left-nav>
+    <!--操作区域-->
+    <div class="main">
+      <router-view/>
+    </div>
+  </div>
+</template>
+
+<script>
+import leftNav from '@/components/common/leftNav'
+export default {
+  name: 'App',
+  components: {
+    leftNav
+  }
+}
+</script>
+
+<style>
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    height: 100%;
+  }
+  .main {
+    float: left;
+    width: 95%;
+    background-color: #EFF2F7;
+    height: 100%;
+    overflow: hidden;
+  }
+</style>
